c29e149acf3423615d5ff7074802530e79816283
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-05-29 22:20:10 +0200

    Update __init__.py

--- bin/paper/__init__.py
+++ bin/paper/__init__.py
@@ -1,11 +1,1 @@
-# paper/__init__.py
 
-from .config_loader import load_config
-from .downloader import download_latest_paper
-from .initializer import start_server_once, apply_eula, run_server_once, run_server_until_generated
-from .velocity_detection import detect_velocity
-from .input_collector import ask_server_properties
-from .property_writer import write_server_properties
-from .config import update_spigot, update_paper_global, update_velocity_toml
-from .service_creator import create_systemd_service
-from .log_monitor import monitor_log_for_warnings
