b3add6406e1cda3d7f4964ba842cefc478165274
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-05-29 21:13:37 +0200

    Update server_finder.py

--- bin/uninstall/server_finder.py
+++ bin/uninstall/server_finder.py
@@ -1,1 +1,10 @@
+from pathlib import Path
 
+BASE_DIR = Path("/opt/minecraft")
+
+def list_paper_servers():
+    return sorted([
+        d.name.replace("paper-", "")
+        for d in BASE_DIR.iterdir()
+        if d.is_dir() and d.name.startswith("paper-")
+    ])
