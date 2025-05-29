a64a7d0d745d238a3a2e5df29b64ff1160ce5ba2
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-05-29 20:56:08 +0200

    Update service_remover.py

--- bin/uninstall/service_remover.py
+++ bin/uninstall/service_remover.py
@@ -1,1 +1,9 @@
+import subprocess
 
+def remove_systemd_service(server_type, name):
+    service_name = f"{server_type}-{name}.service"
+    print(f"➡️  Entferne systemd Service: {service_name}")
+    subprocess.run(["systemctl", "stop", service_name])
+    subprocess.run(["systemctl", "disable", service_name])
+    subprocess.run(["rm", f"/etc/systemd/system/{service_name}"])
+    subprocess.run(["systemctl", "daemon-reload"])
