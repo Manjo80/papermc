bd8804e7bdfac9f10dbf6fa2843fcca9f47333d8
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-05-29 20:47:29 +0200

    Update config_loader.py

--- bin/uninstall/config_loader.py
+++ bin/uninstall/config_loader.py
@@ -1,1 +1,9 @@
+from configparser import ConfigParser
+from pathlib import Path
 
+CONFIG_PATH = Path(__file__).resolve().parent.parent / "config" / "global.conf"
+
+def load_config():
+    config = ConfigParser()
+    config.read(CONFIG_PATH)
+    return config['DEFAULT']
