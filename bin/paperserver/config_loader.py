9ce986f70091b33ac5af091782b9d435fe72ec0d
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-06-02 11:22:38 +0200

    Update config_loader.py

--- bin/paperserver/config_loader.py
+++ bin/paperserver/config_loader.py
@@ -1,8 +1,7 @@
-# paperserver/config_loader.py
 from configparser import ConfigParser
 from pathlib import Path
 
-def load_config():
+def load_config() -> ConfigParser:
     config_path = Path(__file__).resolve().parents[2] / "config" / "global.conf"
     parser = ConfigParser()
     parser.read(config_path)
