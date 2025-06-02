c207e6e5284c95d1583877fb707cec7bfb696181
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-06-02 11:19:51 +0200

    Update config_loader.py

--- bin/paperserver/config_loader.py
+++ bin/paperserver/config_loader.py
@@ -7,3 +7,9 @@ def load_config():
     parser = ConfigParser()
     parser.read(config_path)
     return parser
+
+def load_ram_config():
+    config = load_config()
+    min_ram = config.get("DEFAULT", "MIN_RAM", fallback="512M")
+    max_ram = config.get("DEFAULT", "MAX_RAM", fallback="2G")
+    return min_ram, max_ram
