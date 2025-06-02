bb8a929a1d259704a1ecd6ef97c818c618de9229
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-06-02 09:27:43 +0200

    Update config_loader.py

--- bin/paperserver/config_loader.py
+++ bin/paperserver/config_loader.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 from pathlib import Path
 
 def load_config():
-    config_path = Path(__file__).resolve().parent.parent / "config" / "global.conf"
-    config = ConfigParser()
-    config.read(config_path)
+    config_path = Path(__file__).resolve().parents[2] / "config" / "global.conf"
+    parser = ConfigParser()
+    parser.read(config_path)
     return config['DEFAULT']
