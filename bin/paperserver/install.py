db769ff51284d64a597c170caabf8f3088b88a7c
Manjo80 <121744446+Manjo80@users.noreply.github.com>
2025-06-02 09:26:22 +0200

    Update install.py

--- bin/paperserver/install.py
+++ bin/paperserver/install.py
@@ -2,52 +2,54 @@
 
 import os
 from pathlib import Path
-from bin.paperserver.download_paper import download_latest_paper
-from bin.paperserver.config_loader import load_config
-from bin.paperserver.server_starter import start_server_until_eula, start_until_configs_generated
-from bin.paperserver.config import accept_eula, write_server_properties
-from bin.paperserver.service_creator import create_systemd_service
+from paperserver.download_paper import download_latest_paper
+from paperserver.config_loader import load_config
+from paperserver.server_starter import start_server_until_eula, start_until_configs_generated
+from paperserver.config import accept_eula, write_server_properties
+from paperserver.service_creator import create_systemd_service
 
-# Konfiguration laden
-config = load_config()
+def main():
+   
+    # Konfiguration laden
+    config = load_config()
 
-# Installationsverzeichnis aus global.conf
-base_dir = Path(config['BASE_DIR'])
-base_dir.mkdir(parents=True, exist_ok=True)
+    # Installationsverzeichnis aus global.conf
+    base_dir = Path(config['BASE_DIR'])
+    base_dir.mkdir(parents=True, exist_ok=True)
 
-# Servernamen abfragen
-name = input("➡️ Name des neuen Paper-Servers: ").strip()
-if not name:
-    print("❌ Kein Name eingegeben.")
-    exit(1)
+    # Servernamen abfragen
+    name = input("➡️ Name des neuen Paper-Servers: ").strip()
+    if not name:
+        print("❌ Kein Name eingegeben.")
+        return
 
-server_dir = base_dir / f"paper-{name}"
-if server_dir.exists():
-    print("❌ Serververzeichnis existiert bereits.")
-    exit(1)
-server_dir.mkdir()
+    server_dir = base_dir / f"paper-{name}"
+    if server_dir.exists():
+        print("❌ Serververzeichnis existiert bereits.")
+        return
+    server_dir.mkdir()
 
-# Paper herunterladen
-jar_path = download_latest_paper(server_dir)
-print(f"✅ PaperMC wurde heruntergeladen: {jar_path}")
+    # Paper herunterladen
+    jar_path = download_latest_paper(server_dir)
+    print(f"✅ PaperMC wurde heruntergeladen: {jar_path}")
 
-# Server starten bis EULA erscheint
-start_server_until_eula(server_dir)
+    # Server starten bis EULA erscheint
+    start_server_until_eula(server_dir)
 
-# Accept EULA
-accept_eula(server_dir)
+    # Accept EULA
+    accept_eula(server_dir)
 
-# server.properties schreiben
-write_server_properties(server_path, config)
+    # server.properties schreiben
+    write_server_properties(server_path, config)
 
-# Server stsrten bis spiot und paer-global erstellt sind
-start_until_configs_generated(server_dir)
+    # Server stsrten bis spiot und paer-global erstellt sind
+    start_until_configs_generated(server_dir)
 
-# Systemctl erstellen mit autoupdater
-create_systemd_service(server_name, server_dir)
+    # Systemctl erstellen mit autoupdater
+    create_systemd_service(server_name, server_dir)
 
-# Konfiguration anzeigen
-print("✅ Paper-Server erfolgreich installiert.")
-print(f"➡️  Serververzeichnis: {server_dir}")
-print(f"➡️  Startbefehl: systemctl start paper@{server_name}")
-input("🔁 Drücke [Enter], um zurückzukehren ...")
+    # Konfiguration anzeigen
+    print("✅ Paper-Server erfolgreich installiert.")
+    print(f"➡️  Serververzeichnis: {server_dir}")
+    print(f"➡️  Startbefehl: systemctl start paper@{server_name}")
+    input("🔁 Drücke [Enter], um zurückzukehren ...")
